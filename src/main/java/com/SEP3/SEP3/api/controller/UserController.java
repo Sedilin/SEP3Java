c594fb840fcdcadc33e2578758b5201f0d51aabf
Sedilin <sramkowskilukasz@gmail.com>
2023-04-20 10:28:58 +0200

    GetUsers

--- src/main/java/com/SEP3/SEP3/api/controller/UserController.java
+++ src/main/java/com/SEP3/SEP3/api/controller/UserController.java
@@ -5,10 +5,7 @@ import com.SEP3.SEP3.persistance.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +25,19 @@ public class UserController {
     public ResponseEntity<User> addNewUser(@RequestBody User user)
     {
         User success = userService.addUser(user);
+        if (success != null)
+        {
+            return new ResponseEntity<>(success, HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers()
+    {
+        List<User> success = userService.getAllUsers();
         if (success != null)
         {
             return new ResponseEntity<>(success, HttpStatus.OK);
--- src/main/java/com/SEP3/SEP3/persistance/UserService.java
+++ src/main/java/com/SEP3/SEP3/persistance/UserService.java
@@ -42,4 +42,9 @@ public class UserService {
         return user;
     }
 
+    public List<User> getAllUsers()
+    {
+        return users;
+    }
+
 }
